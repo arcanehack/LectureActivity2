c7b1857356894677ed3d87b2a77293d359ec7258
arcanehack <7907763+arcanehack@users.noreply.github.com>
2022-12-12 00:28:12 -0700

    Initial commit

---
+++ LectureActivity2/LectureActivity2/LectureActivity2/Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LectureActivity2
+{
+    public class Service1 : IService1
+    {
+        public int sumOfDigits(int digits) //modified from source: https://www.codegrepper.com/code-examples/csharp/how+to+add+up+the+digits+of+an+int+c%23 
+        {
+            int sumOfNums = 0; //takes in each single digit from the int digits then adds them together
+            while (digits > 0)
+            {
+                sumOfNums += digits % 10;
+                digits /= 10;
+            }
+            return sumOfNums;
+        }
+        public int numOfVowels(string possibleVowelString)
+        {
+
+            int sumOfVowels = 0; //takes in a string then for each character in the possibleVowelString searches for any vowels
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            
+            foreach (char possibleVowel in possibleVowelString)
+            {
+                Console.WriteLine(possibleVowel);
+                for (int i = 0; i < vowels.Length;  i++)
+                {
+                    if (possibleVowel == vowels[i])
+                        sumOfVowels += 1;
+                }
+            }
+            return sumOfVowels;//returns the sum of vowels that were found in the string
+        }
+        public int quitFunction(string quit)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ LectureActivity2/LectureActivity2/SoapClient/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Hunter White
+ * 1/24/22
+ * ASU ID: 1215077149
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client myClient = new ServiceReference1.Service1Client();
+            string menuOption;
+            do //menu that selects which option you want then sends it to the functions of that option.
+            {
+                Console.WriteLine("Select which option you would like:\n" +
+                                    "[1] Sum of digits\n" +
+                                    "[2] Vowel count\n" +
+                                    "[3] Exit");
+                menuOption = Console.ReadLine();
+
+                if (menuOption == "1")
+                {
+                    Console.WriteLine("Enter string of digits: ");
+                    string numString = Console.ReadLine();
+                    int toInteger;
+                    toInteger = Convert.ToInt32(numString);
+                    Console.WriteLine(myClient.sumOfDigits(toInteger));
+                }
+                else if (menuOption == "2")
+                {
+               
+                    Console.WriteLine("Enter string with vowels: ");
+                    string vowelString = Console.ReadLine();
+                    Console.WriteLine(myClient.numOfVowels(vowelString));
+                 
+                }
+
+
+            } 
+            while (menuOption != "3");
+
+            Console.WriteLine("Exiting");
+            
+            
+
+        }
+    }
+}
